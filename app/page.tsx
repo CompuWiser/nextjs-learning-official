d0922a6e46409337c0dc96e3d382f435dc61e3e5
CompuWiser <asem.kakhi@schoolvoice.com>
2023-10-30 15:32:00 +0300

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import type { Route } from 'next';
 
 export default function Page() {
   return (
@@ -21,7 +22,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href={'/login' as Route}
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
